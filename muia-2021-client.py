b97e9352202f1c7f162ab383650e20609adfe13b
JorgeFeHe <jorge.hormi@gmail.com>
2021-10-21 19:53:13 +0200

    merge ball + avoid

--- muia-2021-client.py
+++ muia-2021-client.py
@@ -90,7 +90,7 @@ def startFuzzy():
     turnR['backward']=fuzz.trapmf(turnR.universe, [0.0, 0.0, 0.1, 0.5])
     turnR['static']= fuzz.trimf(turnR.universe, [0.4, 0.5, 0.6])
     turnR['forward']= fuzz.trapmf(turnR.universe, [0.5, 0.9, 1.0, 1.0])
-    
+
     # Definir conjuntos distancia y velicidad
 
     ballDist['far'] = fuzz.trapmf(ballDist.universe, [0.0, 0.0, 0.1, 0.4])
@@ -162,7 +162,7 @@ def avoid(sonar):
     elif sonar[5] < 0.2:
         lspeed, rspeed = -1, +1
     else:
-        lspeed, rspeed = +0.0, +0.0
+        lspeed, rspeed = +2.0, +2.0
 
     return lspeed, rspeed
 
@@ -183,13 +183,13 @@ def calcula_direccion_perdida(turn):
 
 def seguirBola(coord, turn, area):
     print('Area before: ', area)
-    area = area/45000.0 # 
+    area = area/45000.0 #
     print('Area: ', area)
     lspeed, rspeed=calcula_direccion_perdida(turn) # Empieza a girar por donde perdió la bola por última vez
 
     if(len(coord)>0 and area>=0):
         turn.input['ballDesp'] = coord[0]
-        turn.input['ballDist'] = area 
+        turn.input['ballDist'] = area
         turn.compute()
         out=turn.output
 
@@ -236,11 +236,26 @@ def main():
 
             # Planning
             ##lspeed, rspeed = avoid(sonar)
-            lspeed, rspeed = seguirBola(coord, turn, area)
+            left=min(sonar[14], sonar[15], sonar[0], sonar[1])
+            front=min(sonar[2], sonar[3], sonar[4], sonar[5])
+            right=min(sonar[6], sonar[7], sonar[8], sonar[9])
+            back=min(sonar[10], sonar[11], sonar[12], sonar[13])
+
+            if(front<0.1):
+                lspeed, rspeed = -0.2, -0.2
+            elif(left<0.2):
+                lspeed, rspeed = 0.3, 0.0
+            elif(right<0.2):
+                lspeed, rspeed = 0.0, 0.3
+            elif(back<0.1):
+                lspeed, rspeed = 0.05, 0.05
+            else:
+                lspeed, rspeed = seguirBola(coord, turn, area)
+
             print('lspeed: ', lspeed)
             print('rspeed: ', rspeed)
             # Action
-            setSpeed(clientID, hRobot, lspeed, rspeed)
+            setSpeed(clientID, hRobot, 0, 0)
             time.sleep(0.05)
 
         print('### Finishing...')
